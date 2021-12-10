4ed9068e37e6d2b3984b2e278eda0997746a2aac
Richie Seputro <richie@dev.localdomain>
2021-12-10 13:58:07 +0700

    Created `local` branch, edited config.h and config.mk files
    Yes, I made a `local` branch in every suckless repository where I am going to modify for personal use.
    I edited the config.mk file so st can be compiled on FreeBSD.
    I also edited the config.h file so that TERMMOD is equal to (Mod1Mask|ShiftMask).

--- config.mk
+++ config.mk
@@ -7,8 +7,13 @@ VERSION = 0.8.4
 PREFIX = /usr/local
 MANPREFIX = $(PREFIX)/share/man
 
-X11INC = /usr/X11R6/include
-X11LIB = /usr/X11R6/lib
+# Default X11INC and X11LIB dirs
+#X11INC = /usr/X11R6/include
+#X11LIB = /usr/X11R6/lib
+
+# Modified X11INC and X11LIB dirs to work on FreeBSD
+X11INC = /usr/local/include
+X11LIB = /usr/local/lib
 
 PKG_CONFIG = pkg-config
 
